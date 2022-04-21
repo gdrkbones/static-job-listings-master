86c6e29c78007502518aa2099281efa019fd6350
Julio C. Santiesteban Rodriguez <gdrkbones@gmail.com>
2022-04-21 02:03:11 -0400

    add ts-ignore

--- src/hooks/useFocus.ts
+++ src/hooks/useFocus.ts
@@ -3,6 +3,7 @@ import { useRef } from "preact/hooks"
 const useFocus = () => {
   const htmlElRef = useRef(null)
   const setFocus = () => {
+    /* @ts-ignore: Unreachable code error */
     htmlElRef.current && htmlElRef.current.focus()
   }
 
